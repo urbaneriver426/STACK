3d2acc3ba32ef8957d9e280dc7ad95103103c5e6
urbaneriver426 <45755209+urbaneriver426@users.noreply.github.com>
2019-07-07 23:55:17 +0300

    Update stack_list.py

--- stack_list.py
+++ stack_list.py
@@ -21,3 +21,19 @@ class Stack:
 			return self.stack[self.size()-1]
 		else:
 			return None
+	
+	def balance(self):
+		if self.peek() != ")":
+			return "unbalanced"
+		else:
+			count = 0
+			for i in range(self.size()):
+				x = self.pop()
+				if x == ")":
+					count += 1
+				elif x == "(":
+					count -= 1
+			if count == 0:
+				return "balanced"
+			else:
+				return "unbalanced"
